02a710fd8398e9a431a5230eb90c57ecf02b5641
Penang Teams <raj_ppd@yahoo.com>
2024-09-12 21:52:48 +0800

    ts react starter with auto theme

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+/** @format */
+
+export interface CardProps {
+  title: string;
+  img: any;
+  description: string;
+  cta: string;
+}
---
+++ tailwind.config.cjs
@@ -0,0 +1,26 @@
+/**
+ * @format
+ * @type {import('tailwindcss').Config}
+ */
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        accent: {
+          1: "hsl(var(--color-accent1) / <alpha-value>)",
+          2: "hsl(var(--color-accent2) / <alpha-value>)",
+        },
+        bkg: "hsl(var(--color-bkg) / <alpha-value>)",
+        content: "hsl(var(--color-content) / <alpha-value>)",
+        custom_yellow: { 500: "#E11E0A", 700: "#388098" },
+      },
+      animation: {
+        "spin-slower": "spin 35s ease infinite",
+        "spin-slow": "spin 25s ease-in-out infinite reverse",
+      },
+    },
+  },
+  plugins: [],
+};
