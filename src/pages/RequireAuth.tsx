7d739313d3a37435b90fd6c69026c953bf6a282c
Penang Teams <raj_ppd@yahoo.com>
2024-09-24 00:28:58 +0800

    test ok 2

--- src/pages/RequireAuth.tsx
+++ src/pages/RequireAuth.tsx
@@ -3,12 +3,15 @@
 import { Navigate, Outlet, useLocation } from "react-router-dom";
 
 import useAuth from "../hooks/useAuth";
+import Cookies from "js-cookie";
 
 const RequireAuth = () => {
   const { auth } = useAuth();
   const location = useLocation();
+  const myuser = Cookies.get("myuser");
 
-  return auth?.user ? (
+  // return auth?.user ? (
+  return myuser && myuser.length > 0 ? (
     <Outlet />
   ) : (
     <Navigate to="/login" state={{ from: location }} replace />
